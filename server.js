6301fc090a4d8b3109ac02f7940d3c9278516161
Tharun <tharungowda30115@gmail.com>
2025-02-16 08:00:27 +0530

    server

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
+const server = require("http").createServer(app)
+
+app.use(express.static(path.join(__dirname+"/public")))
+
+server.listen(5000)
