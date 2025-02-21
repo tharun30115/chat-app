de0bb828e46dc66cba38eee0880b9c4fc5a26b4a
Tharun <tharungowda30115@gmail.com>
2025-02-21 22:58:03 +0530

    msg

--- public/app.js
+++ public/app.js
@@ -8,7 +8,6 @@
       if (username.length == 0) {
         return;
       }
-      
       socket.emit("newuser", username);
       uname = username;
       app.querySelector(".join-screen").classList.remove("active");
